0747457600caa3a16c1d43f9313692c88607b4b2
Robert Stiff <uatecuk@gmail.com>
2015-12-06 11:21:19 +0000

    Fork the the control panel from the application itself.

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var Express = require('express'),
     app = new Express(),
-    config = require('./configcontroller.js'),
     bodyParser = require('body-parser'),
-    proxy = require('express-http-proxy');
+    proxy = require('express-http-proxy'),
+    vhost = require('vhost');
 
+var objectAssign = require('object-assign');
 
 // database
 var redis = require("redis"),
@@ -13,67 +14,47 @@ client.on("error", function (err) {
     console.log("Error " + err);
 });
 
-/// Helpers
 
-if (!String.prototype.startsWith) {
-  String.prototype.startsWith = function(searchString, position) {
-    position = position || 0;
-    return this.indexOf(searchString, position) === position;
-  };
-}
+app.use( bodyParser.json() );
 
 // expose static admin UI
-app.use('/_admin', Express.static('public'));
+app.use(Express.static('public'));
 
-// import config controller
-app.use('/_config', config);
-
-// proxy everything else
-app.use('/*', function(req, res, next)
-  {
-    console.log(req.originalUrl);
-    if (req.originalUrl.startsWith('/_')) 
-    {
-      return next();
-    }
-    console.log('looking up config for: ' + req.headers.host);
-
-    client.get(req.headers.host, function(err, mapping) {
-      
-
-      // var mapping = mappings[req.headers.host];
-      var conf = JSON.parse(mapping);
-      console.log('mapping to: ' + conf.host);
-      
-      var failureType = 'failureType' in conf ? conf._failureType : 'end';
-      
-      if ( 'failureRate' in conf )
-      {
-        if ( Math.random() < conf.failureRate )
-        {
-          console.log(failureType);
-          switch (failureType)
-          {
-            case 'end': res.end(); break;
-            case '404': return next();
-          }
-        }
-      }
-      
-      var proxyfunc = proxy(conf.host, {  
-        forwardPath: function(req, res) {
-          console.log(conf.host + req.originalUrl);
-          return conf.host + req.originalUrl;
-        }
-      });
-      
-      (proxyfunc)(req, res, next);
-    });
+app.post('/_config/*', function(req, res, next) {
+  var key = req.originalUrl.replace('/_config/', '');
+  client.set(key, JSON.stringify(req.body));
+  res.end();
 });
 
-
+app.get('/_config/*', function(req, res, next) {
+  var key = req.originalUrl.replace('/_config/', '');
+  if ( key.length )
+  {
+    client.get(key, function(err, resp) {
+      res.send(JSON.parse(resp));
+      res.end();
+    });
+  }
+  else 
+  {
+    client.keys("*", function (err, replies) {
+        client.mget(replies, function(err, resp) {
+          
+          var responses = resp.map(JSON.parse);
+          var output = {};
+          for ( var i = 0; i < responses.length; i++ )
+          {
+            output[replies[i]] = responses[i];
+          }
+          res.send(output);
+          res.end();
+        });
+    });
+  }
+});
+console.log('listning on ' + process.env.PORT);
 app.listen(process.env.PORT || 80);
 
 module.exports = app;
 
-console.log('fatalist started');
+console.log('fatalist.controlpanel started');
